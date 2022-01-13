b476252af9bc55d90b0def0f182b98bdeb7df311
LamarMoore <lamar.moore@stfc.ac.uk>
2022-01-13 07:27:11 +0000

    Replace CXXTest with GTest (#2)

---
+++ code/test/BenchmarksTest.cpp
@@ -0,0 +1,38 @@
+#include <gtest/gtest.h>
+#include <benchmarks.h>
+
+using namespace Benchmarks;
+
+namespace LocalGoogleTests {
+	class BenchmarksTest : public ::testing::Test {
+	};
+
+	TEST_F(BenchmarksTest, DefaultConstruction) {
+		BenchmarkTests benchmark;
+		const size_t defaultSize = 100000;
+		EXPECT_EQ(benchmark.getBenchmarkArraySize(), defaultSize);
+
+		const auto& vec = benchmark.getBenchmarkVector();
+		EXPECT_EQ(vec.size(), defaultSize);
+	}
+
+	TEST_F(BenchmarksTest, ManualConstruction) {
+		const size_t size = 250000;
+		BenchmarkTests benchmark(size);
+		const auto& vec = benchmark.getBenchmarkVector();
+
+		EXPECT_EQ(benchmark.getBenchmarkArraySize(), size);
+		EXPECT_EQ(vec.size(), size);
+	}
+
+	void test_set_benchmark_vector_size() {
+		BenchmarkTests benchmark;
+		const size_t size = 3000;
+
+		benchmark.setBenchmarkArraySize(size);
+		const auto& vec = benchmark.getBenchmarkVector();
+
+		EXPECT_EQ(benchmark.getBenchmarkArraySize(), size);
+		EXPECT_EQ(vec.size(), size);
+	}
+}
