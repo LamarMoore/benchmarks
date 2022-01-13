71b1d321f82ffc0b8a0d28cbcc313c004cd33186
LamarMoore <lamar.moore@stfc.ac.uk>
2022-01-13 15:17:11 +0000

    Separate error extraction from worfklow

---
+++ .github/scripts/extract_cppcheck_errors.sh
@@ -0,0 +1,10 @@
+# Mark build as passed or failed. The additional "|| true" stops the build from failing if there are no errors.
+errors_count=$(grep -c '</error>' ./build/cppcheck.xml) || true
+if [ $errors_count -ne ${ALLOWED_ERRORS_COUNT} ]; then
+  echo "CppCheck found ${errors_count} errors."
+  echo "See CppCheck link on the job page for more detail, or adjust the count."
+  exit 1
+else
+  echo "CppCheck found no errors"
+  exit 0
+fi
