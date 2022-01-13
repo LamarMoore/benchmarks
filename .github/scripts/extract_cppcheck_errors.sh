989046b779f99541e7eddbb007386e9f3da15c83
riteoff <lamar.moore@hotmail.com>
2022-01-13 15:28:28 +0000

    Fix if statement in script

--- .github/scripts/extract_cppcheck_errors.sh
+++ .github/scripts/extract_cppcheck_errors.sh
@@ -1,6 +1,6 @@
 # Mark build as passed or failed. The additional "|| true" stops the build from failing if there are no errors.
 errors_count=$(grep -c '</error>' ./build/cppcheck.xml) || true
-if [ $errors_count -ne ${ALLOWED_ERRORS_COUNT} ]; then
+if [ $errors_count -ne 0 ]; then
   echo "CppCheck found ${errors_count} errors."
   echo "See CppCheck link on the job page for more detail, or adjust the count."
   exit 1
