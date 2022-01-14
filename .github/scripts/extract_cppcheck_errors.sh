69100602d39c93275b47e465727ce5a0547c47e8
riteoff <lamar.moore@hotmail.com>
2022-01-14 07:52:43 +0000

    fix script error

--- .github/scripts/extract_cppcheck_errors.sh
+++ .github/scripts/extract_cppcheck_errors.sh
@@ -1,13 +1,12 @@
 # Mark build as passed or failed. The additional "|| true" stops the build from failing if there are no errors.
-cppcheck_file = "./build/cppcheck.xml"
-errors_count=$(grep -c '</error>' $cppcheck_file) || true
+errors_count=$(grep -c '</error>' ./build/cppcheck.xml) || true
 if [ $errors_count -ne 0 ]; then
   echo "CppCheck found ${errors_count} errors."
   while read_dom; do
     if [[ $ENTITY -eq "error" ]]; then
         echo $CONTENT
     fi
-  done < $cppcheck_file
+  done < ./build/cppcheck.xml
   exit 1
 else
   echo "CppCheck found no errors"
